731cd1f8147dc8d1c548c4f20c1a8067f0f1de9a
“bushitan” <373514952@qq.com>
2020-02-08 16:04:11 +0800

    v1.2.11 修改海报SDK，支持本地图片上传

--- components/index/index.js
+++ components/index/index.js
@@ -315,10 +315,15 @@ const helper = {
      */
     _downImage(imageUrl) {
         return new Promise((resolve, reject) => {
-            if (/^http/.test(imageUrl) && !new RegExp(wx.env.USER_DATA_PATH).test(imageUrl)) {
+            if (/^http:\/\/tmp\//.test(imageUrl)) {
+                resolve(imageUrl);
+            } else if (
+                /^http/.test(imageUrl) &&
+                !new RegExp(wx.env.USER_DATA_PATH).test(imageUrl)
+            ) {
                 wx.downloadFile({
                     url: this._mapHttpToHttps(imageUrl),
-                    success: (res) => {
+                    success: res => {
                         if (res.statusCode === 200) {
                             resolve(res.tempFilePath);
                         } else {
@@ -327,7 +332,7 @@ const helper = {
                     },
                     fail(err) {
                         reject(err);
-                    },
+                    }
                 });
             } else {
                 // 支持本地地址
